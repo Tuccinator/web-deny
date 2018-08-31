6aa374c7ca99f6f00aa925a18f43aaad21358896
Tuccinator <nicktucci@hotmail.ca>
2018-08-31 03:54:47 -0400

    add/remove intermittent and permanent sites

--- js/options.js
+++ js/options.js
@@ -1,32 +1,65 @@
 // load the blocked list DOM element
-const $blockedSites = document.getElementById('blocked-sites');
+const $intermittentBlockedSites = document.getElementById('intermittent-blocked-sites');
+const $intermittentAddSite = document.getElementById('intermittent-add-site');
+const $permanentBlockedSites = document.getElementById('permanent-blocked-sites');
+const $permanentAddSite = document.getElementById('permanent-add-site');
 
-// check the keystroke from the URL blocker
-document.querySelector('input').addEventListener('keyup', e => {
+document.body.addEventListener('click', e => {
+	// check if the click is from a blocked site to remove it
+	if(e.target.classList.contains('blocked-site-remove')) {
+		removeBlockedSite(e.target['data-url']);
+		e.preventDefault();
+	}
+})
 
+const modal = new VanillaModal.default();
+
+$intermittentAddSite.addEventListener('click', e => {
+	modal.open('#intermittent-modal');
+});
+
+$permanentAddSite.addEventListener('click', e => {
+	modal.open('#permanent-modal');
+});
+
+function onKeyup(e) {
 	// when enter is pressed, add to the blocked site list
 	if(e.which == 13) {
-		loadBlockedSites()
-			.then(sites => {
-				let site = {};
+		let site = {};
 
-				// automatically add the HTTP/S prefix
-				const url = addHttpPrefix(e.target.value);
+		// automatically add the HTTP/S prefix
+		const url = addHttpPrefix(e.target.value);
 
-				site[e.target.value] = {
-					url: url,
-					type: 1,
-					favicon: url + 'favicon.ico'
-				};
+		// automatically set the site type to permanent
+		let type = 'permanent';
 
-				// persist the updated blocked site list to the local storage
-				browser.storage.local.set(site);
-			})
-			.then(() => createBlockedList())
-			.then(() => e.target.value = '')
-		return false;
+		// check if the site is only intermittently blocked
+		if(e.target.id === 'intermittent-input') {
+			type = 'intermittent';
+		}
+
+		site[e.target.value] = {
+			url: url,
+			type: 1,
+			favicon: `https://s2.googleusercontent.com/s2/favicons?domain_url=${url}`,
+			type: type,
+			added_at: Date.now()
+		};
+
+		// persist the updated blocked site list to the local storage
+		browser.storage.local.set(site);
+
+		// create the new lists for each category
+		createBlockedList();
+
+		// reset the input
+		e.target.value = '';
 	}
-});
+}
+
+// check the keystroke from the URL blockers
+document.getElementById('intermittent-input').addEventListener('keyup', onKeyup);
+document.getElementById('permanent-input').addEventListener('keyup', onKeyup);
 
 // load all of the blocked sites from the local storage
 function loadBlockedSites() {
@@ -40,6 +73,7 @@ function addHttpPrefix(s) {
 	if (s.substr(0, prefix.length) !== prefix) {
 	    s = prefix + '://' + s;
 	}
+
 	if(s.substr(-1, 1) !== '/') {
 		s += '/';
 	}
@@ -50,14 +84,35 @@ function addHttpPrefix(s) {
 // add the blocked sites to the DOM
 function createBlockedList() {
 	loadBlockedSites()
-		.then(sites => {
-			$blockedSites.innerHTML = '';
+		.then(unsortedSites => {
+			// grab the keys from the storage object
+			const unsortedKeys = Object.keys(unsortedSites);
 
-			const siteKeys = Object.keys(sites);
-			for(const siteKey of siteKeys) {
-				const site = sites[siteKey];
+			// create a new array to store the site objects
+			const unsortedSitesTransformed = [];
+
+			// go through each object key and get the site matching the key
+			for (const unsortedKey of unsortedKeys) {
+				const unsortedSite = unsortedSites[unsortedKey];
+
+				// add the site to the array
+				unsortedSitesTransformed.push(unsortedSite);
+			}
+
+			// sort the array by date descending
+			return unsortedSitesTransformed.sort((a, b) => b.added_at - a.added_at);
+		})
+		.then(sites => {
+
+			$intermittentBlockedSites.innerHTML = '';
+			$permanentBlockedSites.innerHTML = '';
+
+			for (const site of sites) {
 
 				// create url element
+				const siteElementContainer = document.createElement('div');
+				siteElementContainer.className = 'blocked-site-container';
+
 				const siteElement = document.createElement('div');
 				siteElement.className = 'blocked-site';
 
@@ -65,20 +120,51 @@ function createBlockedList() {
 				const siteFavicon = document.createElement('img');
 				siteFavicon.src = site.favicon;
 
+				const siteDeleter = document.createElement('a');
+				siteDeleter['data-url'] = site.url;
+				siteDeleter.className = 'blocked-site-remove';
+				siteDeleter.text = 'x';
+				siteDeleter.href = '#';
+
 				// create the url text
 				const siteText = document.createTextNode(site.url);
 
 				// add the favicon and text to the url
 				siteElement.append(siteFavicon);
+				siteElement.append(siteDeleter);
 				siteElement.append(siteText);
 
-				// add the url to the blocked sites list
-				$blockedSites.append(siteElement);
+				siteElementContainer.append(siteElement);
+
+				// add the url to the appropriate blocked sites list
+				if(site.type === 'intermittent') {
+					$intermittentBlockedSites.append(siteElementContainer);
+				} else {
+					$permanentBlockedSites.append(siteElementContainer);
+				}
 			}
 		})
 		.then(() => {
 			let backgroundPage = browser.extension.getBackgroundPage();
 			backgroundPage.resetBlockedSites();
+		})
+}
+
+function removeBlockedSite(site) {
+	loadBlockedSites()
+		.then(sites => {
+
+			// transform the URL into an anchor tag to get the hostname
+			const link = document.createElement('a');
+			link.href = site;
+
+			// make sure the URL exists in the first place
+			if(sites[link.host] !== undefined) {
+				browser.storage.local.remove(link.host);
+			}
+		})
+		.then(() => {
+			createBlockedList();
 		})
 }
 
